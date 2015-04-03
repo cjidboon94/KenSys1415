07449892516aa1e0c739fa63a1d716357f70d03c
Cornelis Boon <killazelda@gmail.com>
2015-04-03 13:52:32 +0200

    Organized code so it is more readable

--- KS1.pl
+++ KS1.pl
@@ -28,18 +28,33 @@ is_a(thingy, bird).
 %%%%%%%%%%%%%%%%%%%
 is_a(blahtje, bird).
 
-is_a_rec(Child,Parent):- concept(Child), concept(Parent), is_a(Child, X), is_a_rec(X, Parent), concept(X).
+is_a_rec(Child,Parent):- 
+	concept(Child), concept(Parent), 
+	is_a(Child, X), 
+	is_a_rec(X, Parent), 
+	concept(X).
 
 % wrapper rule, collects all the properties of the parent
-is_a(Child, Parent):- concept(Child), concept(Parent), Parent \= Child, bagof([Type,Value], has(Parent, Type, Value), ParentAtributes), is_a2(Child, ParentAtributes).
+is_a(Child, Parent):- 
+	concept(Child), concept(Parent), 
+	Parent \= Child, 
+	bagof([Type,Value], has(Parent, Type, Value), ParentAtributes), 
+	is_a2(Child, ParentAtributes).
 
 % base case, all atributes have been checked
 is_a2(_, []):- !.
 
 % checks for all the properties in the parent whether the child has the same value for the properties
-is_a2(Child, [[Type,Value]|Tail]):- has(Child, Type, Value), !, is_a2(Child, Tail).
+is_a2(Child, [[Type,Value]|Tail]):- 
+	has(Child, Type, Value), !, 
+	is_a2(Child, Tail).
 
 % specific rule inheritance rule for ranges
-is_a2(Child, [[Type,between(LowerValue, UpperValue)]|Tail]):- has(Child, Type, Value), between(LowerValue, UpperValue, Value), !, is_a2(Child, Tail).
+is_a2(Child, [[Type,between(LowerValue, UpperValue)]|Tail]):- 
+	has(Child, Type, Value), 
+	between(LowerValue, UpperValue, Value), !, 
+	is_a2(Child, Tail).
 
-show(X):- bagof([Value, Type], (has(X, Value, Type); is_a(X, Parent), has(Parent, Value, Type)), Content), print(Content).
+show(X):- 
+	bagof([Value, Type], (has(X, Value, Type); is_a(X, Parent), has(Parent, Value, Type)), Content), 
+	print(Content).
