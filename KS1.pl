852bd4168b7f39051761e16d9704b1c78d76268c
Koen van der Keijl <kvanderkeyl@gmail.com>
2015-04-05 14:02:03 +0200

    added ability to add concepts

--- KS1.pl
+++ KS1.pl
@@ -5,6 +5,8 @@
 % Date: 31-3-2015
 
 % concepts
+:- dynamic concept/1.
+
 concept(mammal).
 concept(bird).
 concept(thing).
@@ -28,9 +30,11 @@ has(bird, wings, true).
 
 has(thingy, wings, true).
 
-is_a(blahtje, something).
-
 has(Child, Value, Type):- concept(Child), is_a_rec(Child, Parent), concept(Parent), has(Parent, Value, Type).
+
+has_all(Concept, Content):- bagof([Value,Type], has(Concept, Value, Type), Content).
+
+is_a(blahtje, something).
 
 
 %%%%%%%%%%%%%%%%%%%
@@ -40,7 +44,7 @@ is_a_rec(Child,Parent):- is_a(Child, Parent).
 is_a_rec(Child,Parent):- 
     concept(Parent), 
     concept(Child), is_a(Child, Z), 
-    is_a_rec(Z, Parent), concept(X).
+    is_a_rec(Z, Parent), concept(Z).
 
 % wrapper rule for is_a
 is_a_wrapper(Child, Parent):- is_a_rec(Child, Parent).
@@ -52,8 +56,8 @@ is_a2(_, []):- !.
 
 % checks for all the properties in the parent whether the child has the same value for the properties
 is_a2(Child, [[Type,Value]|Tail]):- 
-	has(Child, Type, Value), !, 
-	is_a2(Child, Tail).
+    has(Child, Type, Value), !,
+    is_a2(Child, Tail).
 
 % specific rule inheritance rule for ranges
 is_a2(Child, [[Type,between(LowerValue, UpperValue)]|Tail]):- 
@@ -61,6 +65,14 @@ is_a2(Child, [[Type,between(LowerValue, UpperValue)]|Tail]):-
     between(LowerValue, UpperValue, Value), !, 
     is_a2(Child, Tail).
 
-has_all(Child, Content):- bagof([Value,Type], has(Child, Value, Type), Content).
+is_all(Concept, Content):- bagof(Parent, is_a_wrapper(Concept, Parent), Content).
 
-show(X):- has_all(X, Content), print(Content).
+%%%%%%%%%%%%%%%%%%%%%
+% database operations
+%%%%%%%%%%%%%%%%%%%%%
+
+% shows the content of a concept
+show(Concept):- has_all(Concept, Content), is_all(Concept, Content2), print(Content), print(Content2).
+
+% adds a new rule
+add_concept(Concept):- \+concept(Concept), assert(concept(Concept)).
