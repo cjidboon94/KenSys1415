d24fbc1fb5c158975e2a39acb8085c607d0816cb
Cornelis Boon <killazelda@gmail.com>
2015-04-14 12:51:49 +0200

    Rules defined

--- assignment3/db.pl
+++ assignment3/db.pl
@@ -3,10 +3,26 @@
 %% Cornelis Boon - 10561145       %%%%
 %% Database voor opdracht 3       %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- op(800, fx, if).
+:- op(750, xfx, else).
+:- op(700, xfx, then).
+:- op(300, xfy, or).
+:- op(200, xfy, and).
+
 
 
 %%% Illnesses %%%
+%% Malaria 
+if oorzaak(muggensteek) and koorts then malaria.
+if malaria and aanvallen(regelmatig, _) then goedaardige_malaria else malaria_tropica.
+if goedaardige_malaria and aanvallen(regelmatig, 2) then malaria_tertiana.
+if goedaardige_malaria and aanvallen(regelmatig, 3) then malaria_quartana.
 
-
+%%darminfectie
+if diarree then darminfectie.
+if darminfectie and has(ontlatsting, bloed, slijm) then dysenterie.
+if darminfectie and geling then geelzucht.
+if darminfectie and verlamming then polio.
 
 %%%% Symptoms %%%
+if lichaampstemperatuur > 38 then koorts.
---
+++ assignment3/template.pl
@@ -0,0 +1,54 @@
+/* --- Defining operators --- */
+
+:- op(800, fx, if).
+:- op(700, xfx, then).
+:- op(300, xfy, or).
+:- op(200, xfy, and).
+
+
+/* --- A simple backward chaining rule interpreter --- */
+
+is_true( P ):-
+    fact( P ).
+
+is_true( P ):-
+    if Condition then P,
+    is_true( Condition ).
+
+is_true( P1 and P2 ):-
+    is_true( P1 ),
+    is_true( P2 ).
+
+is_true( P1 or P2 ):-
+    is_true( P1 )
+    ;
+    is_true( P2 ).
+
+
+/* --- A simple forward chaining rule interpreter --- */
+
+forward:-
+    new_derived_fact( P ),
+    !,
+    write( 'Derived:' ), write_ln( P ),
+    assert( fact( P )),
+    forward
+    ;
+    write_ln( 'No more facts' ).
+
+new_derived_fact( Conclusion ):-
+    if Condition then Conclusion,
+    not( fact( Conclusion ) ),
+    composed_fact( Condition ).
+
+composed_fact( Condition ):-
+    fact( Condition ).
+
+composed_fact( Condition1 and Condition2 ):-
+    composed_fact( Condition1 ),
+    composed_fact( Condition2 ).
+
+composed_fact( Condition1 or Condition2 ):-
+    composed_fact( Condition1 )
+    ;
+    composed_fact( Condition2 ).
