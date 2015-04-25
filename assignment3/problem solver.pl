938d371bf95e83c5e45a7072f6f0e7b3f82b4c5b
Koen van der Keijl <kvanderkeyl@gmail.com>
2015-04-25 21:50:45 +0200

    modifed rules

---
+++ assignment3/problem solver.pl
@@ -0,0 +1,67 @@
+/* --- Defining operators --- */
+
+:- op(800, fx, if).
+:- op(700, xfx, then).
+:- op(200, xfy, and).
+
+:- dynamic fact/1.
+:- dynamic fact/2.
+:- consult('db.pl').
+
+ask_for_additional_wrapper:- ask_for_additional(start).
+
+ask_for_additional(stop):- !.
+ask_for_additional(start):- !, write('geef een andere symptoom aan, of zeg, stop: '),read(Symptom), ask_for_additional(Symptom).
+ask_for_additional(LastSymptom):- !, write('geef een andere symptoom aan, of zeg, stop: '),read(Symptom), ask_for_additional(Symptom), assert(fact(Symptom(LastSymptom), true)).
+
+
+fact(nothing).
+
+is_true( P ):-
+    \+fact( P, false),
+    fact( P, true ).
+
+is_true(vraag(Symptom)):-
+    fact(Symptom);
+    (\+fact(Symptom),
+    write('heeft u ook last van: '), write(Symptom),
+    write('\n'), read(X),
+    X = yes,
+    assert(fact(Symptom))).
+
+is_true( P ):-
+    if Condition then P,
+    is_true( Condition ).
+
+is_true( P1 and P2 ):-
+    is_true( P1 ),
+    is_true( P2 ).
+
+
+/* --- A simple forward chaining rule interpreter --- */
+
+forward:-
+    new_derived_fact( P ),
+    !,
+    write( 'Derived:' ), write_ln( P ),
+    assert( fact( P )),
+    forward
+    ;
+    write_ln( 'No more facts' ).
+
+new_derived_fact( Conclusion ):-
+    if Condition then Conclusion,
+    not( fact( Conclusion ) ),
+    composed_fact( Condition ).
+
+composed_fact( Condition ):-
+    fact( Condition ).
+
+composed_fact( Condition1 and Condition2 ):-
+    composed_fact( Condition1 ),
+    composed_fact( Condition2 ).
+
+composed_fact( Condition1 or Condition2 ):-
+    composed_fact( Condition1 )
+    ;
+    composed_fact( Condition2 ).
