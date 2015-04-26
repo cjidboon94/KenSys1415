122f1e8b836ce54d272ae85a2730a1627217cf75
Koen van der Keijl <kvanderkeyl@gmail.com>
2015-04-26 23:19:59 +0200

    db comments

--- assignment3/db.pl
+++ assignment3/db.pl
@@ -7,7 +7,7 @@
 :- op(700, xfx, then).
 :- op(200, xfy, and).
 
-%Terminals
+/*** Diseases ***/
 ziekte(malaria_tropica).
 ziekte(malaria_tertiana).
 ziekte(malaria_quartana).
@@ -15,36 +15,43 @@ ziekte(dysenterie).
 ziekte(geelzucht).
 ziekte(polio).
 
+/*** Disease Classes ***/
 ziekteklasse(malaria).
 ziekteklasse(darminfectie).
 ziekteklasse(goedaardige_malaria).
 
-%%% Ziektes %%%
-%% Malaria
+
+/*** Disease Rules ***/
+
+/** Malaria **/
 if koorts then ziekteklasse(malaria).
 if ziekteklasse(malaria) and vraag(aanvallen, regelmatig, 'Heeft u aanvallen? (regelmatig/onregelmatig): ') then ziekteklasse(goedaardige_malaria).
 if ziekteklasse(malaria) and vraag(aanvallen, onregelmatig, 'Heeft u aanvallen? (regelmatig/onregelmatig): ') then ziekte(malaria_tropica).
-if ziekteklasse(goedaardige_malaria) and vraag(aanvallen, 2, 'Hoe vaak heeft u aanvallen?') then ziekte(malaria_tertiana).
+if ziekteklasse(goedaardige_malaria) and vraag(aanvallen, 2, 'Hoe vaak heeft u aanvallen? ') then ziekte(malaria_tertiana).
 if ziekteklasse(goedaardige_malaria) and vraag(aanvallen, 3) then ziekte(malaria_quartana).
 
-%% Darminfectie
+/** Darm infection **/
 if diarree then ziekteklasse(darminfectie).
-if ziekteklasse(darminfectie) and vraag(slijm,  'Zit er slijm in uw ontlasting? (true/false):') and vraag(bloed, 'Zit er bloed in uw ontlasting? (true/false):') then ziekte(dysenterie).
-if ziekteklasse(darminfectie) and vraag(geling, 'Is uw huid geel? (true/false):') then ziekte(geelzucht).
-if ziekteklasse(darminfectie) and vraag(verlamming, 'Kunt u zich bewegen? (true/false):') then ziekte(polio).
+if ziekteklasse(darminfectie) and vraag(slijm,  'Zit er slijm in uw ontlasting? (true/false): ') and vraag(bloed, 'Zit er bloed in uw ontlasting? (true/false): ') then ziekte(dysenterie).
+if ziekteklasse(darminfectie) and vraag(geling, 'Is uw huid geel? (true/false): ') then ziekte(geelzucht).
+if ziekteklasse(darminfectie) and vraag(verlamming, 'Kunt u zich bewegen? (true/false): ') then ziekte(polio).
 
-%%%% Symptomen Abstractie %%%
+
+/*** Abstraction ***/
 if (temperatuur, hoog) then koorts.
 
+/* abstract values */
 is_abtract(tempertuur, hoog).
 is_abstract(aanvallen, regelmatig).
 is_abstract(aanvallen, onregelmatig).
 
+/* tries to convert a value to a number */
 convert_to_value(Waarde, IntWaarde):-
 	\+number(Waarde), atom_number(Waarde, IntWaarde).
 
 convert_to_value(Waarde, Waarde):- number(Waarde).
 
+/* abstracts data */
 fact_from_abstract(temperatuur, hoog):-
 	fact(temperatuur, Waarde),
 	convert_to_value(Waarde, IntWaarde),
