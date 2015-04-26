b09a2457c74f2afa497354cf02613b53d16cfdf8
Koen van der Keijl <kvanderkeyl@gmail.com>
2015-04-26 15:08:08 +0200

    eeerste query systeem klaar

--- assignment3/db.pl
+++ assignment3/db.pl
@@ -7,9 +7,6 @@
 :- op(700, xfx, then).
 :- op(200, xfy, and).
 
-%Equality checker
-oorzaak(Oorzaak, Oorzaak).
-
 %Terminals
 ziekte(malaria_tropica).
 ziekte(malaria_tertiana).
@@ -25,10 +22,10 @@ ziekteklasse(goedaardige_malaria).
 %%% Ziektes %%%
 %% Malaria
 if koorts then ziekteklasse(malaria).
-if ziekteklasse(malaria) and vraag(aanvallen, onregelmatig) then ziekteklasse(goedaardige_malaria).
-if ziekteklasse(malaria) and (aanvallen, regelmatig) then ziekte(malaria_tropica).
-if ziekteklasse(goedaardige_malaria) and (aanvallen, 2) then ziekte(ziektemalaria_tertiana).
-if ziekteklasse(goedaardige_malaria) and (aanvallen, 3) then ziekte(malaria_quartana).
+if ziekteklasse(malaria) and vraag(aanvallen, regelmatig) then ziekteklasse(goedaardige_malaria).
+if ziekteklasse(malaria) and vraag(aanvallen, onregelmatig) then ziekte(malaria_tropica).
+if ziekteklasse(goedaardige_malaria) and vraag(aanvallen, '2') then ziekte(ziektemalaria_tertiana).
+if ziekteklasse(goedaardige_malaria) and vraag(aanvallen, '3') then ziekte(malaria_quartana).
 
 %% Darminfectie
 if diarree then ziekteklasse(darminfectie).
@@ -39,12 +36,16 @@ if darminfectie and verlamming then polio.
 %%%% Symptomen Abstractie %%%
 if (temperatuur, hoog) then koorts.
 
-abstract(temperatuur, hoog):-
+is_abtract(tempertuur, hoog).
+is_abstract(aanvallen, regelmatig).
+is_abstract(aanvallen, onregelmatig).
+
+fact_from_abstract(temperatuur, hoog):-
     fact(temperatuur, Waarde), atom_number(Waarde, IntWaarde), IntWaarde >= 38.
 
-abstract(aanvalen, regelmatig):- fact(aanvallen, Waarde), atom_number(Waarde, IntWaarde), IntWaarde >= 0.
+fact_from_abstract(aanvallen, regelmatig):- fact(aanvallen, Waarde), atom_number(Waarde, IntWaarde), IntWaarde >= 0.
 
-abstract(aanvallen, onregelmatig):- fact(aanvallen, Waarde), atom_number(Waarde, IntWaarde), IntWaarde < 0.
+fact_from_abstract(aanvallen, onregelmatig):- fact(aanvallen, Waarde), atom_number(Waarde, IntWaarde), IntWaarde < 0.
 
 % If koorts and malaria ask for 'aanvallen :regelmatig of onregelmatig)'
 % If regelmatig, ask for periods (i.e. every 2 or 3 days) and assert aanvallen(regelmatig, Period).
